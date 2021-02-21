daa9fd2f5088a7ff4b7c2830adf515b92ce627a3
tonifibonacci <antoniobcorreia94@gmail.com>
2021-02-21 20:39:57 +0000

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,24 @@
 import random
+ 
 
 def main():
-  roll = random.randint(1,6)
-  print('you rolled a', roll)
+  dice_rolls=2
+  dice_sum=0
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum=dice_sum + roll
+    print('you rolled a', roll)
+  print('the sum of them is', dice_sum)
+    
+
+
 
 if __name__== "__main__":
   main()
+
+
+
+
 
 
 
